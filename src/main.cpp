3303161669ee9c191f185647eef15b65a94d5719
bayueweiyang20 <2920259214@qq.com>
2024-09-21 20:22:47 +0800

    初步了解窗口及渲染流程

--- src/main.cpp
+++ src/main.cpp
@@ -2,46 +2,65 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    glViewport(0, 0, width, height);
-}
+//回调函数，在每次窗口大小被调整的时候被调用，使视口大小跟随窗口大小变化
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+//输入控制，若按下了返回键(Esc)，程序在下次循环关闭
+void processInput(GLFWwindow *window);
+
+//设置长宽
+const unsigned int WIDTH = 800;
+const unsigned int HEIGHT = 600;
 
 int main() {
-    // 初始化GLFW
-    if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW\n";
-        return -1;
-    }
 
-    // 创建窗口
-    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", NULL, NULL);
-    if (!window) {
-        std::cerr << "Failed to create GLFW window\n";
+    glfwInit();//初始化GLFW，为后续GLFW操作做全局设置
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);//主版本号
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);//次版本号
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);//指定 OpenGL 上下文的 配置文件
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL);//创建一个窗口并设置窗口大小和名称
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
+    glfwMakeContextCurrent(window);//通知GLFW将我们窗口的上下文设置为当前线程的主上下文
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);//告诉GLFW我们希望每当窗口调整大小的时候调用
 
-    glfwMakeContextCurrent(window);
-
-    // 加载OpenGL函数
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cerr << "Failed to initialize GLAD\n";
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))//GLAD是用来管理OpenGL的函数指针，在调用任何OpenGL的函数之前我们需要初始化GLAD
+    {
+     std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    // 设置视口
-    glViewport(0, 0, 800, 600);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    //渲染循环
+    while(!glfwWindowShouldClose(window))
+    {
+        //输入
+        processInput(window);//按键控制，Esc退出程序
 
-    // 渲染循环
-    while (!glfwWindowShouldClose(window)) {
+        //渲染
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);//在新渲染前需要清屏
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+        //检查并调用事件，交换缓冲
+        glfwSwapBuffers(window);//交换颜色缓冲，用来绘制
+        glfwPollEvents();//检查有没有触发什么事件（比如键盘输入、鼠标移动等）、更新窗口状态，并调用对应的回调函数    
     }
 
-    // 释放资源
-    glfwTerminate();
+    glfwTerminate();//渲染结束后释放资源
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);//设置视口，即渲染窗口的尺寸，前两个参数控制窗口左下角的位置。第三个和第四个参数控制渲染窗口的宽度和高度
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
