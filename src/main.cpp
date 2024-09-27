119e9ed4099807b142452f08b564d56ecda41efe
bayueweiyang20 <2920259214@qq.com>
2024-09-27 19:52:27 +0800

    观察矩阵lookat

--- src/main.cpp
+++ src/main.cpp
@@ -227,7 +227,11 @@ int main() {
         
         // 观察矩阵
         glm::mat4 view = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f)); // 注意，我们将矩阵向我们要进行移动场景的反方向移动。
+        // view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f)); // 注意，我们将矩阵向我们要进行移动场景的反方向移动。
+        float radius = 10.0f;
+        float camX = sin(glfwGetTime()) * radius;
+        float camZ = cos(glfwGetTime()) * radius;
+        view = glm::lookAt(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
         // 投影矩阵
         glm::mat4 projection = glm::mat4(1.0f);
